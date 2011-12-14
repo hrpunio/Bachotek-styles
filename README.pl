d9dc6eb24de798cbc41ed2fb47035c326487063d
tomasz przechlewski <rudolf.von.ems@gmail.com>
2011-12-14 10:39:59 +0100

    first commit

---
+++ README.pl
@@ -0,0 +1,23 @@
+Papers in English should be prepared with standard ltugproc.cls/tugproc.sty macros
+
+==================================================================================
+
+Teksty w jêzyku polskim nale¿y przygotowaæ pos³uguj±c siê zmodyfikowanymi
+wersjami makrodefinicji ltugproc.cls/tugproc.sty, tj.:
+
+ -- ltugboatp.cls, ltugprocp.cfg, ltugprocp.cls (latex, zalecany)
+ -- tugboatp.sty, tugprocp.cfg, tugprocp.sty (plain)
+
+Je¿eli korzystamy z LaTeX-a to piszemy tak, jak w przypadku pos³ugiwania
+siê klas± article. Przyk³ady s± w plikach example-1-latex.tex, example-2-latex.tex
+
+Je¿eli wykorzystujemy `tugprocp', to piszemy tak, jak w przypadku stylu `gust'.  
+Przyk³ady s± w plikach example-1-plain.tex, example-2-plain.tex 
+
+Opis poleceñ stylu `gust' znajduje siê w artykule Staszka Wawrykiewicza
+,,Krótki przewodnik jak pisaæ do Biuletynu GUST''
+ftp.gust.org.pl/TeX/GUST/bulletin/06/08-sw1.ps
+
+==================================================================================
+
+--t.przechlewski // luty 2001, marzec 2005, luty 2010 //
